487b81e4c0556c24ccc65bb8ed754b2e2d062582
furkat88 <sattorov.furkat88@gmail.com>
2022-09-04 17:52:58 +0500

    done

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const burger = document.querySelector('.burger')
 const bar1 = document.querySelector('.bar1')
 const bar2 = document.querySelector('.bar2')
 const bar3 = document.querySelector('.bar3')
+const menu = document.querySelector('ul')
 
 burger.addEventListener('click', () => {
     bar1.classList.toggle('change')
     bar2.classList.toggle('change')
     bar3.classList.toggle('change')
+
+    menu.classList.toggle('hidden')
 })
